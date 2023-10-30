13af96732fd7983a40c8fc5de7902f9a638e35c9
rachidel12 <elrachidmorabit@gmail.com>
2023-10-30 17:42:05 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# autism_detection
+this is an android mobile app for detecting autism using Deep Learning.
+<br />
+We built the model using the transfer learning and the code for creating the model is in the notebook.
+<br /> 
+<br />
+Then we converted the model to a tensorflow lite format so the mobile application can use it. We built the app using Android Studio.
---
+++ source_code_java/app/src/main/java/com/example/projet_dl/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.projet_dl;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import com.example.projet_dl.ml.Model0;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button camera, gallery;
+    ImageView imageView;
+    TextView result;
+    int imageSize = 224;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.button);
+        gallery = findViewById(R.id.button2);
+
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imageView);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 3);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(cameraIntent, 1);
+            }
+        });
+    }
+
+    public void classifyImage(Bitmap image){
+        try {
+            Model0 model = Model0.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
+            for(int i = 0; i < imageSize; i ++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model0.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            float prediction = confidences[0];  // Assuming outputFeature0 has a single prediction
+
+            String resultText;
+            if (prediction >= 0.5) {
+                resultText = "Autist";
+            } else {
+                resultText = "Not Autist";
+            }
+
+            String notresultText= "";
+            if(resultText == "Autist"){
+                notresultText="Not Autist";
+            }else {
+                notresultText="Autist";
+            }
+
+            if (prediction >= 0.5) {
+                result.setText(resultText+ ": "+ Float.toString(prediction*100) + "% \n"
+                        + notresultText+ ": "+ Float.toString(100 - prediction*100) + "% \n");
+            } else {
+                result.setText(notresultText+ ": "+ Float.toString(prediction*100) + "% \n"
+                        + resultText+ ": "+ Float.toString(100- prediction*100) + "% \n");
+            }
+//            result.setText(resultText+ ": "+ Float.toString(prediction*100) + "% \n"
+//                            + notresultText+ ": "+ Float.toString(prediction*100) + "% \n");
+
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK){
+            if(requestCode == 3){
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }else{
+                Uri dat = data.getData();
+                Bitmap image = null;
+                try {
+                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+}
